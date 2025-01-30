4c5aee0396a51c156a90f76d917fa7df5e4bf418
George Perry <george.e.perry5@gmail.com>
2025-01-30 11:09:06 -0800

    working version with adequate layout

--- client/src/BingoCard.js
+++ client/src/BingoCard.js
@@ -1,27 +1,59 @@
+// import React from 'react'
+// import BingoCell from './BingoCell'
+
+// function BingoCard ({ bingoGrid }) {
+//   const columns = ['B', 'I', 'N', 'G', 'O']
+//   return (
+//     <table className='bingo-table'>
+//       <thead>
+//         <tr>
+//           {columns.map(col => (
+//             <th key={col}>{col}</th>
+//           ))}
+//         </tr>
+//       </thead>
+//       <tbody>
+//         {bingoGrid.map((row, rowIndex) => (
+//           <tr key={rowIndex}>
+//             {row.map((cellText, colIndex) => (
+//               <BingoCell key={colIndex} text={cellText} />
+//             ))}
+//           </tr>
+//         ))}
+//       </tbody>
+//     </table>
+//   )
+// }
+
+// export default BingoCard
+
 import React from 'react'
 import BingoCell from './BingoCell'
 
 function BingoCard ({ bingoGrid }) {
   const columns = ['B', 'I', 'N', 'G', 'O']
+
   return (
-    <table className='bingo-table'>
-      <thead>
-        <tr>
-          {columns.map(col => (
-            <th key={col}>{col}</th>
-          ))}
-        </tr>
-      </thead>
-      <tbody>
-        {bingoGrid.map((row, rowIndex) => (
-          <tr key={rowIndex}>
-            {row.map((cellText, colIndex) => (
-              <BingoCell key={colIndex} text={cellText} />
+    <div className='bingo-panel'>
+      <table className='bingo-table'>
+        <thead>
+          <tr>
+            {columns.map(col => (
+              <th key={col}>{col}</th>
             ))}
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {bingoGrid.map((row, rowIndex) => (
+            <tr key={rowIndex}>
+              {row.map((cellText, colIndex) => (
+                <BingoCell key={colIndex} text={cellText} />
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   )
 }
 
